5b21e79a3f863bd63c83a8745f4ec275563bec0a
jolocdj <alfredjolodejesus@gmail.com>
2026-04-28 17:06:30 +0800

    update works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -157,7 +157,7 @@ const projects = [
     ],
   },
   {
-    title: "Achievo (Work in Progress)",
+    title: "Achievo - Task Management Web App",
     description:
       "Achievo is a productivity and task management web application designed to help users organize their daily activities, track progress, and stay focused on their goals. The platform provides an intuitive dashboard where users can manage tasks, monitor schedules through a calendar view, and keep track of ongoing work in a structured and visually appealing interface.",
     image: "/Proj-ACV.mp4",
